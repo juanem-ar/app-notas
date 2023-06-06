47a73149c36eaf7194f9a567cf0a3455ff5c23ca
Selien Yorbandi <selienyorbandi@gmail.com>
2023-06-06 17:20:38 -0300

    feat: Add model for 'Nota'

---
+++ app/src/main/java/com/ispc/notas/models/NotaModel.java
@@ -0,0 +1,39 @@
+package com.ispc.notas.models;
+
+public class Note {
+    private long id;
+    private String title;
+    private String description;
+
+    public Note() {
+    }
+
+    public Note(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
